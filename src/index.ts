8a0274463292284ca9c45b239346426f0209b9c7
sofia_liggio <sofialiggio994@gmail.com>
2024-07-19 16:46:09 +0200

    created new app

---
+++ src/index.ts
@@ -0,0 +1,87 @@
+// #region ::: IMPORTS :::
+import { createClient } from "@vercel/postgres";
+import express, { Request, Response } from "express";
+import { config } from "dotenv";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import cors from "cors";
+import path from "path";
+// #endregion
+
+// #region ::: CONFIGURATION :::
+config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const client = createClient({
+  connectionString: process.env.DATABASE_URL,
+});
+
+client.connect();
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+// #endregion
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("message-sent", (message) => {
+    client.query(
+      `INSERT INTO messages (content) VALUES ($1)`,
+      [message],
+      (error) => {
+        if (!error) io.emit("message-received", message);
+      }
+    );
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/messages", (req: Request, res: Response) => {
+  client.query("SELECT * FROM messages", (error, response) => {
+    if (error) res.status(500).json({ error });
+    else res.status(200).json(response.rows);
+  });
+});
+
+app.post("/api/messages", (req: Request, res: Response) => {
+  const { content } = req.body;
+  client.query(
+    `INSERT INTO messages (content) VALUES ($1)`,
+    [content],
+    (error) => {
+      if (error) res.status(500).json({ error });
+      else res.status(200).json({ message: "Message created successfully" });
+    }
+  );
+});
+
+server.listen(PORT, () => {
+  console.log(`Server API is running http://localhost:${PORT}`);
+});
